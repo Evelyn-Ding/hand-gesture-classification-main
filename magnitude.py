21cb34d9180ae688c69ac87900fde61dd2d472d8
Evelyn Ding <evelyn.s.ding@gmail.com>
2022-10-01 14:14:00 -0500

    Added code to track magnitude of tremors

---
+++ magnitude.py
@@ -0,0 +1,8 @@
+# list to keep track of things
+# function to do the math
+
+
+# objective: create a time-domain graph that tracks the magnitude with respect to time (like tracking acceleration wrt time)
+# the magnitude is the difference between two (x,y) coordinates between 2 frames
+# magnitude is calculated as sqrt(x^2+y^2) and captures the change in the two points (as 1 Dimension)
+# magnitude allows you to see how much the hands has moved in order to track the severity of the tremor 
